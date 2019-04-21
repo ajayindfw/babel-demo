843063de7d41ce9518fc4feff8919dd622e8e3d1
ajaygupta0512 <ajayindfw@gmail.com>
2019-04-21 11:24:18 -0400

    Initial files

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import {operations} from "./arithmetic.js";
+
+let greeting = "hi";
+console.log(`-- ${greeting} Craig`);
+
+let addResult = operations.add(2, 3);
+console.log(addResult);
+
+let subtractResult = operations.subtract(2, 3);
+console.log(subtractResult);
